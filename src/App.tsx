b2c802d87c381544b72def5d3b6a8a20866cc30f
sing2gather <loic.lalevee@gmail.com>
2021-07-28 18:07:07 +0200

    Configure Sentry source maps in a small TypeScript project

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function throwAnErrorInTypescript() {
+  throw new Error('This is an Error in TypeScript !');
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button type="button" onClick={() => throwAnErrorInTypescript()}>Throw an Error in TypeScript</button>
       </header>
     </div>
   );
